9b353d95fdb39bf492c18789e5352d278265a9f5
Matthew J Grant <mathiasschnell@gmail.com>
2025-12-27 02:08:36 -0800

    Changed help from a flag to a command. Refined each command's help screen.

--- src/cli.php
+++ src/cli.php
@@ -4,9 +4,16 @@ require_once __DIR__ . '/cache_funcs.php';
 require_once __DIR__ . '/command_funcs.php';
 require_once __DIR__ . '/help_funcs.php';
 
+$options = getopt("v", ["version"]);
+$valid_commands = ['query', 'info'];
 $argv = $_SERVER['argv'];
 $argc = $_SERVER['argc'];
 $cache_dir = __DIR__ . '/../cache/';
+
+if(isset($options['v']) || isset($options['version'])) :
+    echo "off_search v1.0\n";
+    exit(0);
+endif;
 
 if(!is_dir($cache_dir)):
     mkdir($cache_dir, 0755, true);
@@ -20,6 +27,11 @@ endif;
 $command = strtolower(trim($argv[1]));
 
 switch($command):
+    case "help":
+        $subcom = strtolower(trim(!in_array($argv[2] ?? '', $valid_commands) ? 'main' : $argv[2]));
+        $help_func = 'off_search_' . $subcom . '_help';
+        $help_func();
+        break;
     case "query":
         handle_query($argc, $argv);
         break;
